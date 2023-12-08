60127970cb0f2b5f7d3f8ff7af8d3513a410bd2a
noor hamdan <124586788+noorhamdan0@users.noreply.github.com>
2023-12-09 01:39:43 +0200

    ext_upd2

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json({ limit: '10mb' })); 
+app.use(express.json());
 
-
-const externalRoutes = require('./routes/externalRoutes');
-
-app.use('/api/external', externalRoutes);
-
+const userRoutes = require('./routes/userRoutes');
+const messageRoutes = require('./routes/messageRoutes');
+const dataSourceRoutes = require('./routes/dataSourceRoutes');
+const dataUploadRoutes = require('./routes/dataUploadRoutes');
+const environmentalDataRoutes = require('./routes/environmentalDataRoutes');
+app.use('/api/users', userRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/dataSources', dataSourceRoutes);
+app.use('/api/dataUpload', dataUploadRoutes); 
+app.use('/api/environmentalData', environmentalDataRoutes);
+console.log('Additional statement for noorBranch');
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
