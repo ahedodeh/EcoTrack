0ffecabb65468efd46089542c34e93f7b8eb3fe6
noor hamdan <124586788+noorhamdan0@users.noreply.github.com>
2023-12-09 01:12:50 +0200

    external_api
    external_api

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' })); 
 
 const userRoutes = require('./routes/userRoutes');
 const messageRoutes = require('./routes/messageRoutes');
 const dataSourceRoutes = require('./routes/dataSourceRoutes');
 const dataUploadRoutes = require('./routes/dataUploadRoutes');
 const environmentalDataRoutes = require('./routes/environmentalDataRoutes');
+const externalRoutes = require('./routes/externalRoutes');
+
 app.use('/api/users', userRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/dataSources', dataSourceRoutes);
 app.use('/api/dataUpload', dataUploadRoutes); 
-app.use('/api/environmentalDataRoutes', environmentalDataRoutes);
+app.use('/api/environmentalData', environmentalDataRoutes);
+app.use('/api/external', externalRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
---
+++ models/external.js
@@ -0,0 +1,78 @@
+const axios = require('axios');
+
+const getWeatherData = async (city, country) => {
+  const apiKey = '7520f34dcf2c4a53bef13518230312';
+  const apiUrl = 'https://api.weatherapi.com/v1/current.json';
+
+  try {
+    const response = await axios.get(apiUrl, {
+      params: {
+        key: apiKey,
+        q: `${city},${country}`,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching weather API: ${error.message}`);
+  }
+};
+
+const getOpenWeatherMapData = async (city, country) => {
+  const apiKey = '54c670aef2e4dc50697c989f76b1a370';
+  const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+  try {
+    const response = await axios.get(apiUrl, {
+      params: {
+        q: `${city},${country}`,
+        appid: apiKey,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching OpenWeatherMap API: ${error.message}`);
+  }
+};
+
+
+
+
+const getAirQualityDataByZipCode = async (zipCode) => {
+  try {
+    const apiUrl = `https://www.airnowapi.org/aq/observation/zipCode/current/?format=text/csv&zipCode=${zipCode}&distance=25&API_KEY=4DF01500-C3FD-4056-A022-0B6BBE1124E1`;
+
+    const response = await axios.get(apiUrl);
+
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching air quality data by zip code: ${error.message}`);
+  }
+};
+
+
+const getUVIndexData = async (latitude, longitude) => {
+  const apiUrl = `https://api.openuv.io/api/v1/uv?lat=${latitude}&lng=${longitude}`;
+  const apiKey = 'openuv-16cmrsrlprip02a-io';
+
+  try {
+    const response = await axios.get(apiUrl, {
+      headers: {
+        'x-access-token': apiKey,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching UV Index API: ${error.message}`);
+  }
+};
+
+
+module.exports = {
+    getWeatherData,
+    getAirQualityDataByZipCode,
+    getOpenWeatherMapData,
+    getUVIndexData,
+};
