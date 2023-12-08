2d74aae9f7dc7abcf27bfb454a133890be7f81d1
noor hamdan <124586788+noorhamdan0@users.noreply.github.com>
2023-12-09 01:22:30 +0200

    ext_update

--- index.js
+++ index.js
@@ -6,18 +6,9 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json({ limit: '10mb' })); 
 
-const userRoutes = require('./routes/userRoutes');
-const messageRoutes = require('./routes/messageRoutes');
-const dataSourceRoutes = require('./routes/dataSourceRoutes');
-const dataUploadRoutes = require('./routes/dataUploadRoutes');
-const environmentalDataRoutes = require('./routes/environmentalDataRoutes');
+
 const externalRoutes = require('./routes/externalRoutes');
 
-app.use('/api/users', userRoutes);
-app.use('/api/messages', messageRoutes);
-app.use('/api/dataSources', dataSourceRoutes);
-app.use('/api/dataUpload', dataUploadRoutes); 
-app.use('/api/environmentalData', environmentalDataRoutes);
 app.use('/api/external', externalRoutes);
 
 app.listen(port, () => {
