2da0180829458b48a6a7e4c966bbaa80f2162b8f
unknown <ahedodeh@outlook.com>
2023-12-09 02:41:30 +0200

    open data

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const externalRoutes = require('./routes/externalRoutes');
 const educationalResourceRoutes = require('./routes/educationalResourceRoutes');
 const reportRoutes = require('./routes/reportRoutes');
 const sustainabilityScoreRoutes = require('./routes/sustainabilityScoreRoutes');
+const openDataRoutes = require('./routes/openDataRoutes');
 app.use('/api/users', userRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/dataSources', dataSourceRoutes);
@@ -24,6 +25,7 @@ app.use('/api/external', externalRoutes);
 app.use('/api/educationalResource', educationalResourceRoutes);
 app.use('/api/report', reportRoutes);
 app.use('/api/sustainability', sustainabilityScoreRoutes);
+app.use('/api/openData', openDataRoutes);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
