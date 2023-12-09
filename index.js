0d59055b50bbd8c899011922829ff8505da27606
unknown <ahedodeh@outlook.com>
2023-12-09 03:46:07 +0200

    Revert "updated index,js"
    This reverts commit db1417e29ad7896dbdc12995d6ecaafe269a83f0.

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ app.use('/api/educationalResource', educationalResourceRoutes);
 app.use('/api/report', reportRoutes);
 app.use('/api/sustainability', sustainabilityScoreRoutes);
 app.use('/api/openData', openDataRoutes);
+
+console.log("main page");
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
