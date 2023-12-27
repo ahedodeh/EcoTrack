ee02af6f352b1e1e08e6e102a4a062f44dce2519
unknown <ahedodeh@outlook.com>
2023-12-28 01:07:58 +0200

    Update dataSource

--- controllers/dataSourceController.js
+++ controllers/dataSourceController.js
@@ -3,79 +3,35 @@ const DataSourceModel = require('../models/dataSource');
 exports.createDataSource = (req, res) => {
   const { sourceName, sourceType } = req.body;
 
-
-  DataSourceModel.createDataSource(sourceName, sourceType, (err, results) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
-    } else {
-      res.status(201).json({ message: 'Data Source created successfully' });
-    }
+  DataSourceModel.createDataSource(sourceName, sourceType, (result) => {
+    res.status(result.status).json({ message: result.message });
   });
 };
 
-exports.getDataSource = (req, res) => {
-  const sourceID = req.params.sourceid;
 
-  DataSourceModel.getDataSource(sourceID, (err, results) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
-    } else {
-      if (results.length > 0) {
-        res.status(200).json(results[0]);
-      } else {
-        res.status(404).json({ message: 'Data Source not found' });
-      }
-    }
-  });
-};
 
 exports.updateDataSource = (req, res) => {
   const sourceID = req.params.sourceid;
   const { sourceName, sourceType } = req.body;
 
-  DataSourceModel.getDataSource(sourceID, (err, results) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
-    } else {
-      if (results.length > 0) {
-        DataSourceModel.updateDataSource(sourceID, sourceName, sourceType, (err, results) => {
-          if (err) {
-            console.error(err);
-            res.status(500).json({ error: 'Internal server error' });
-          } else {
-            res.status(200).json({ message: 'Data Source updated successfully' });
-          }
-        });
-      } else {
-        res.status(404).json({ message: 'Data Source not found' });
-      }
-    }
+  DataSourceModel.updateDataSource(sourceID, sourceName, sourceType, (result) => {
+    res.status(result.status).json({ message: result.message });
+  });
+};
+
+
+exports.getDataSource = (req, res) => {
+  const sourceName = req.params.sourceid;
+
+  DataSourceModel.getDataSource(sourceName, (result) => {
+    res.status(result.status).json({ message: result.message });
   });
 };
 
 exports.deleteDataSource = (req, res) => {
-  const sourceID = req.params.sourceid;
+  const sourceName = req.params.sourceid;
 
-  DataSourceModel.getDataSource(sourceID, (err, results) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
-    } else {
-      if (results.length > 0) {
-        DataSourceModel.deleteDataSource(sourceID, (err, results) => {
-          if (err) {
-            console.error(err);
-            res.status(500).json({ error: 'Internal server error' });
-          } else {
-            res.status(200).json({ message: 'Data Source deleted successfully' });
-          }
-        });
-      } else {
-        res.status(404).json({ message: 'Data Source not found' });
-      }
-    }
+  DataSourceModel.deleteDataSource(sourceName, (result) => {
+    res.status(result.status).json({ message: result.message });
   });
 };
--- models/dataSource.js
+++ models/dataSource.js
@@ -1,30 +1,98 @@
-// dataSource.js (model)
 const db = require('../config/db');
 
 exports.createDataSource = (sourceName, sourceType, callback) => {
-  const sql = 'INSERT INTO data_sources (source_name, source_type) VALUES (?, ?)';
-  const values = [sourceName, sourceType];
+  const checkDuplicateQuery = 'SELECT COUNT(*) AS count FROM data_sources WHERE source_name = ?';
+  const checkDuplicateValues = [sourceName];
 
-  db.query(sql, values, callback);
+  db.query(checkDuplicateQuery, checkDuplicateValues, (checkErr, checkResult) => {
+    if (checkErr) {
+      return callback({ status: 500, message: 'Internal server error' });
+    }
+
+    if (checkResult[0].count > 0) {
+      return callback({ status: 409, message: 'Source name is already in use' });
+    }
+
+    const insertQuery = 'INSERT INTO data_sources (source_name, source_type) VALUES (?, ?)';
+    const insertValues = [sourceName, sourceType];
+
+    db.query(insertQuery, insertValues, (insertErr, insertResult) => {
+      if (insertErr) {
+        return callback({ status: 500, message: 'Internal server error' });
+      }
+
+      return callback({ status: 201, message: 'Data source created successfully' });
+    });
+  });
 };
 
-exports.getDataSource = (sourceID, callback) => {
-  const sql = 'SELECT * FROM data_sources WHERE source_id = ?';
-  const values = [sourceID];
+exports.getDataSource = (sourceName, callback) => {
+  const sql = 'SELECT * FROM data_sources WHERE source_name = ?';
+  const values = [sourceName];
 
-  db.query(sql, values, callback);
+  db.query(sql, values, (err, results) => {
+    if (err) {
+      return callback({ status: 500, message: 'Internal server error' });
+    }
+
+    if (results.length > 0) {
+      return callback({ status: 200, message: results[0] });
+    } else {
+      return callback({ status: 404, message: 'Data Source not found' });
+    }
+  });
 };
+
+exports.deleteDataSource = (sourceName, callback) => {
+  const checkExistenceQuery = 'SELECT COUNT(*) AS count FROM data_sources WHERE source_name = ?';
+  const checkExistenceValues = [sourceName];
+
+  db.query(checkExistenceQuery, checkExistenceValues, (checkErr, checkResult) => {
+    if (checkErr) {
+      return callback({ status: 500, message: 'Internal server error' });
+    }
+
+    if (checkResult[0].count > 0) {
+      const deleteQuery = 'DELETE FROM data_sources WHERE source_name = ?';
+      const deleteValues = [sourceName];
+
+      db.query(deleteQuery, deleteValues, (deleteErr, deleteResult) => {
+        if (deleteErr) {
+          return callback({ status: 500, message: 'Internal server error' });
+        }
+
+        return callback({ status: 200, message: 'Data Source deleted successfully' });
+      });
+    } else {
+      return callback({ status: 404, message: 'Data Source not found' });
+    }
+  });
+};
+
 
 exports.updateDataSource = (sourceID, sourceName, sourceType, callback) => {
-  const sql = 'UPDATE data_sources SET source_name = ?, source_type = ? WHERE source_id = ?';
-  const values = [sourceName, sourceType, sourceID];
+  const checkDuplicateQuery = 'SELECT COUNT(*) AS count FROM data_sources WHERE source_name = ? AND source_name != ?';
+  const checkDuplicateValues = [sourceName, sourceID];
 
-  db.query(sql, values, callback);
+  db.query(checkDuplicateQuery, checkDuplicateValues, (checkErr, checkResult) => {
+    if (checkErr) {
+      return callback({ status: 500, message: 'Internal server error' });
+    }
+
+    if (checkResult[0].count > 0) {
+      return callback({ status: 409, message: 'New source name is already in use' });
+    }
+
+    const updateQuery = 'UPDATE data_sources SET source_name = ?, source_type = ? WHERE source_name = ?';
+    const updateValues = [sourceName, sourceType, sourceID];
+
+    db.query(updateQuery, updateValues, (updateErr, updateResult) => {
+      if (updateErr) {
+        return callback({ status: 500, message: 'Internal server error' });
+      }
+
+      return callback({ status: 200, message: 'Data source updated successfully' });
+    });
+  });
 };
 
-exports.deleteDataSource = (sourceID, callback) => {
-  const sql = 'DELETE FROM data_sources WHERE source_id = ?';
-  const values = [sourceID];
-
-  db.query(sql, values, callback);
-};
